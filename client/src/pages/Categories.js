c9fb0a21780ebb87641719dc0f7e1e828ce1423e
Atianna DeAngelo <atianna.deangelo@gmail.com>
2021-07-10 15:53:32 -0600

    init

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -56,29 +56,29 @@
 // }
 
 // export default Categories
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Card, Carousel, Image } from 'react-bootstrap'
+// import axios from 'axios'
+// import React, { useEffect, useState } from 'react'
+// import { Card, Carousel, Image } from 'react-bootstrap'
 
-const Categories = () => {
-  const [categories, setCategories] = useState([])
+// const Categories = () => {
+//   const [categories, setCategories] = useState([])
 
-  useEffect(()=>{
-  getCategories()
-  },[])
+//   useEffect(()=>{
+//   getCategories()
+//   },[])
 
-  const getCategories = async ()=>{
-  try{
-  let res = await axios.get(`/api/categories`)
-  setCategories(res.data[0])
-  }catch (err){
-  alert('err check console')
-  console.log(err)
-  }
-}
+//   const getCategories = async ()=>{
+//   try{
+//   let res = await axios.get(`/api/categories`)
+//   setCategories(res.data[0])
+//   }catch (err){
+//   alert('err check console')
+//   console.log(err)
+//   }
+// }
 
-return(
-  <div>CAAAATS</div>
-  )
-}
-export default Categories
+// return(
+//   <div>CAAAATS</div>
+//   )
+// }
+// export default Categories
