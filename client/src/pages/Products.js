d837e8a8d92a6acf5a078a0d1400f9d5a9f05127
Kat Duff <katrina348@gmail.com>
2021-07-10 15:44:04 -0600

    routes fixed

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,43 +1,43 @@
-// import axios from 'axios'
-// import React, { useEffect, useState } from 'react'
-// import { Card, Carousel, Image } from 'react-bootstrap'
+import React,{useEffect, useState} from 'react'
+import axios from 'axios'
 
-// const Products = () => {
-//     const [products, setProducts] = useState([])
+const Products = () => {
+  const [products, setProducts] = useState([])
 
-//     useEffect(()=>{
-//     getProducts()
-//     },[])
+  useEffect(()=>{
+    getProducts()
+  },[])
 
-//     const getProducts = async ()=>{
-//     try{
-//     let res = await axios.get(`/api/products`)
-//     setProducts(res.data[0])
-//     }catch (err){
-//     alert('err check console')
-//     console.log(err)
-//   }}
-//   // create_table "products", force: :cascade do |t|
-//   // t.string "name"
-//   // t.string "image"
-//   // t.float "price"
-//   // t.text "description"
-//   // t.boolean "shopping_list"
-//   const renderProducts = () => {
-//     return products.map(test=> {
-//       return (
-//         //each child in a list needs to have a unique key.
-//         <div key={products.id}>
-//             <h1>Products</h1>
-//             <h2>{products.name}</h2>
-//             <h2>{products.image}</h2>
-//             <h3>{products.price}</h3>
-//             <h3>{products.description}</h3>
-//             <h3>{products.shopping_list}</h3>
-//             {renderProducts()}
-//         </div>
-//       )
-//     })
-//   }}
+  const getProducts = async () => {
+    try{
+      let res = await axios.get(`/api/categories/:category_id/products`)
+      setProducts(res.data)
+    }catch (err){
+      alert('err check console')
+      console.log(err)
+    }
+  }
 
-//   export default Products
+  const renderProducts = () => {
+    return products.map(product=> {
+      return (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <h4>{product.image}</h4>
+          <h4>{product.price}</h4>
+          <h4>{product.description}</h4>
+          <h4>{product.shopping_list}</h4>
+        </div>
+      )
+    })
+  }
+      
+      return(
+        <div style={{textAlign: 'center'}}>
+      <h1>Products</h1>
+      {renderProducts()}
+      </div>
+      )
+    }
+
+export default Products
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
-    
     resources :rooms do
-      get '/rooms', to: 'rooms#index'
-      resources :categories do 
-        get 'categories', to: 'categories#index'
-        resources :products do
-          get '/products', to: 'products#index'
-        end 
+      resources :categories
+    end 
+    resources :categories do
+      resources :products do
       end
     end
   end
