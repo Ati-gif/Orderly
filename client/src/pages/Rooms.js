7af2cd4adc4cfe114e84f8ba46937e2e959ef050
Atianna DeAngelo <atianna.deangelo@gmail.com>
2021-07-10 16:17:24 -0600

    init

--- client/src/pages/Rooms.js
+++ client/src/pages/Rooms.js
@@ -1,26 +1,30 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Card, Carousel, Image } from 'react-bootstrap'
+// import React, {useState} from 'react'
+// import Items from '../item/Items'
+// import StoreForm from './StoreForm'
 
-const Rooms = () => {
-  const [rooms, setRooms] = useState([])
+// const Rooms = (props) => {
+//   const {id, name, editRoom, deleteRoom} = props
+//   const [showForm, setShowForm] = useState(false)
+//   const [showCategory, setShowCategory] = useState(false)
 
-  useEffect(()=>{
-  getRooms()
-  },[])
+ 
 
-  const getRooms = async ()=>{
-  try{
-  let res = await axios.get(`/api/rooms`)
-  setRooms(res.data[0])
-  }catch (err){
-  alert('err check console')
-  console.log(err)
-  }
-}
-
-return(
-  <div>ROOOOOMS</div>
-  )
-}
-export default Rooms
+//   return(
+//     <div>
+//       <div>
+//         <h2 >{name}</h2>
+//         <h3></h3>
+//         <h3></h3>
+//         <button onClick={()=>setShowForm(!showForm)}>Edit Room</button>
+//         <button onClick={()=>deleteRoom(id)}>Delete</button>
+//         <p></p>
+//       </div>
+//       {showForm && <RoomForm {...props} editStore={editStore} setShowForm={setShowForm}/>}
+//       <p></p>
+//       <button onClick={()=>setShowItem(!showItem)}>View Items</button>
+//       {showItem && <Items store_id={id}/>}
+//       <p></p>
+//     </div>
+//   )
+// }
+// export default Rooms
